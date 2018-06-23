bc2254220814300cda7b0c97610acbb1a381a30b
Damian Kaczmarek <rush@rushbase.net>
2018-06-22 22:44:15 -0500

    Check pushing to git

--- index.js
+++ index.js
@@ -1,16 +1,41 @@
 const Promise = require('bluebird');
 const ghpages = require('gh-pages');
 
-Promise.promisifyAll(ghpages);
+const childProcess = require('child_process')
 
-ghpages.publishAsync('dist', {
-  branch: 'release',
-  push: true,
-  tag: 'dupa',
-  user: {
-    name: 'Drone',
-    email: 'rush@rushbase.net'
+Promise.promisifyAll(childProcess);
+
+const { execFileAsync } = childProcess;
+
+const TEMPORARY_BRANCH = 'publish-to-git-temporary';
+
+async function run({tag, push}) {
+  try {
+    await execFileAsync('git', ['branch', '-D', TEMPORARY_BRANCH]);
+  } catch(err) {}
+
+  const out = await execFileAsync('git', ['branch', TEMPORARY_BRANCH]);
+  const commitId = (await execFileAsync('git',  ['subtree', 'split', '--prefix', 'dist', TEMPORARY_BRANCH])).trim();
+  await execFileAsync('git', ['tag', tag, commitId]);
+
+  if (push) {
+    const out = await execFileAsync('git', ['push', push.remote || 'origin', tag]);
+    console.log(out);
   }
-}).then(() => {
-  console.log('DONE');
+
+  await execFileAsync('git', ['branch', '-D', TEMPORARY_BRANCH]);
+}
+
+run({
+  tag: 'v1.1',
+  push: {
+    remote: 'origin'
+  }
+}).catch(err => {
+  if(err.cmd) {
+    console.error(err.message);
+  } else {
+    console.error(err);
+  }
+  process.exit(1);
 });
